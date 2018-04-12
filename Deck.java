dcae653b1971cebdacb8a0c036193278e7f879c0
James Meadows 18 <JamesMeadows18@newmanschool.org>
2018-04-12 08:18:09 -0500

    commit

---
+++ Deck.java
@@ -0,0 +1,143 @@
+package javaapplication3;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * The Deck class represents a shuffled deck of cards.
+ * It provides several operations including
+ *      initialize, shuffle, deal, and check if empty.
+ */
+public class Deck {
+
+	/**
+	 * cards contains all the cards in the deck.
+	 */
+	private List<Card> cards;
+
+	/**
+	 * size is the number of not-yet-dealt cards.
+	 * Cards are dealt from the top (highest index) down.
+	 * The next card to be dealt is at size - 1.
+	 */
+	private int size;
+
+
+	/**
+	 * Creates a new <code>Deck</code> instance.<BR>
+	 * It pairs each element of ranks with each element of suits,
+	 * and produces one of the corresponding card.
+	 * @param ranks is an array containing all of the card ranks.
+	 * @param suits is an array containing all of the card suits.
+	 * @param values is an array containing all of the card point values.
+	 */
+	public Deck(String[] ranks, String[] suits, int[] values) {
+		this.cards = new ArrayList<Card>();
+		for (int j = 0; j < ranks.length; j++) {
+			for (String suitString : suits) {
+				cards.add(new Card(ranks[j], suitString, values[j]));
+			}
+		}
+		this.size = this.cards.size();
+		this.shuffle();
+	}
+
+
+	/**
+	 * Determines if this deck is empty (no undealt cards).
+	 * @return true if this deck is empty, false otherwise.
+	 */
+	public boolean isEmpty() {
+		return this.cards.isEmpty();
+	}
+
+	/**
+	 * Accesses the number of undealt cards in this deck.
+	 * @return the number of undealt cards in this deck.
+	 */
+	public int size() {
+		return this.cards.size();
+	}
+
+	/**
+	 * Randomly permute the given collection of cards
+	 * and reset the size to represent the entire deck.
+	 */
+        
+    private Random random = new Random();
+    
+    public void shuffle(){
+        selectionShuffle();
+    }
+    
+    public void selectionShuffle(){
+        for (int k = 0; k < cards.size()-1; k++){
+            int j = random.nextInt(cards.size());
+            Card c = cards.get(j);
+            cards.set(k, c);
+        }
+    }
+    
+    public void perfectShuffle(){
+        int k = 0;
+        for (int j = 0; j < 25; j++){
+            Card c = cards.get(j);
+            cards.set(k, c);
+            k += 2;
+        }
+        k = 1;
+        for (int j = 26; j < 51; j++){
+            Card c = cards.get(j);
+            cards.set(k, c);
+            k += 2;
+        }
+    }
+
+	/**
+	 * Deals a card from this deck.
+	 * @return the card just dealt, or null if all the cards have been
+	 *         previously dealt.
+	 */
+	public Card deal() {
+            if (cards.isEmpty())return null;
+            
+            Card card = cards.get(0);
+            cards.remove(card);
+            return card;
+	}
+
+	/**
+	 * Generates and returns a string representation of this deck.
+	 * @return a string representation of this deck.
+	 */
+	@Override
+	public String toString() {
+		String rtn = "size = " + size + "\nUndealt cards: \n";
+
+		for (int k = size - 1; k >= 0; k--) {
+			rtn = rtn + cards.get(k);
+			if (k != 0) {
+				rtn = rtn + ", ";
+			}
+			if ((size - k) % 2 == 0) {
+				// Insert carriage returns so entire deck is visible on console.
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\nDealt cards: \n";
+		for (int k = cards.size() - 1; k >= size; k--) {
+			rtn = rtn + cards.get(k);
+			if (k != size) {
+				rtn = rtn + ", ";
+			}
+			if ((k - cards.size()) % 2 == 0) {
+				// Insert carriage returns so entire deck is visible on console.
+				rtn = rtn + "\n";
+			}
+		}
+
+		rtn = rtn + "\n";
+		return rtn;
+	}
+}
